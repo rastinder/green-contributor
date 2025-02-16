2f70d907aaa71cce6f7431aad11c2fdabb595b19
Automated Contributor <automated@example.com>
2025-02-16 11:25:00 +0530

    feat: Add new function at 2025-02-16T05:55:00.013Z

--- active_code.py
+++ active_code.py
@@ -4,3 +4,8 @@ def fibonacci(n):
     if n <= 1:
         return n
     return fibonacci(n-1) + fibonacci(n-2)
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
