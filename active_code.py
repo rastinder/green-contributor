89e369ba0b19a7334949a26542be82d0bc677690
Automated Contributor <automated@example.com>
2025-02-23 13:28:00 +0530

    refactor: Remove function at 2025-02-23T07:58:00.019Z

--- active_code.py
+++ active_code.py
@@ -1,10 +1,3 @@
-def fibonacci(n):
-    if n <= 1:
-        return n
-    return fibonacci(n-1) + fibonacci(n-2)
-
-
-
 def binary_search(arr, target):
     left, right = 0, len(arr) - 1
     while left <= right:
@@ -16,6 +9,7 @@ def binary_search(arr, target):
         else:
             right = mid - 1
     return -1
+
 
 
 
